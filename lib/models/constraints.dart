0ab1e653c4df53edbea63f66865ea7c6fc646810
fedigmd1 <gammoudifedi@gmail.com>
2019-12-23 12:21:01 +0100

    add screens& models& and add keys & constraint & crypto

---
+++ lib/models/constraints.dart
@@ -0,0 +1,3 @@
+class Constraints {
+  static const baseUrl = 'http://gateway.marvel.com';
+}
---
+++ lib/models/keys.dart
@@ -0,0 +1,4 @@
+class Keys {
+  static const publicKey = "f287ea7c9cf8b11621f1f8d64a9b7c79";
+  static const privateKey = "e97b67c374271c8189538a6e3f2cce415d3deb17";
+}
