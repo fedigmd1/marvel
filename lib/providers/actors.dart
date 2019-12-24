f7b9a760a886cdc4ecf59564d3d47f1390e86a70
fedigmd1 <gammoudifedi@gmail.com>
2019-12-24 11:07:59 +0100

    add some models

---
+++ lib/providers/actors.dart
@@ -0,0 +1,16 @@
+import '../models/thumbnail.dart';
+
+class Character {
+  int id;
+  String name;
+  String description;
+  Thumbnail thumbnail;
+
+  Character(
+      {this.id,
+        this.name,
+        this.description,
+        this.thumbnail});
+
+        
+}
